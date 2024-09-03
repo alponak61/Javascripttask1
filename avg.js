dcb233bc33f92f023648f53b6b6b868c8a65f362
alpona <mstalponak61@gmail.com>
2024-09-04 00:35:43 +0600

    subject average mark

---
+++ avg.js
@@ -0,0 +1,10 @@
+ var mathmatics = 75.25;
+ var biology = 65;
+ var chemistry = 80;
+ var physics = 35.45;
+ var bangla = 99.50;
+
+ var total = mathmatics + biology + chemistry + physics + bangla;
+ var total = parseFloat(total);
+ var average = total / 5;
+ console.log(average.toFixed(2));
